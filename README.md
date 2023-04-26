0065b48400cd35861f946ef614e153c16c20865a
wirudesu <wj.deleon19@gmail.com>
2023-04-26 08:04:19 +0800

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# TicTacToe
-TicTacToe repo
--- login_system/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'username', 'password', 'database_name');
-
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-?>
